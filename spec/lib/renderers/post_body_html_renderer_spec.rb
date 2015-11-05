74f6ab78de97c6fd2c9a9c8fae71db2e6bb2d2e5
Nick Cherry <ncherry@cipherhealth.com>
2015-11-04 19:13:38 -0500

    add specs for PostBodyHtmlRenderer

---
+++ spec/lib/renderers/post_body_html_renderer_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe Renderers::PostBodyHtmlRenderer do
+
+  let(:parser) { Parsers::PostBodyMarkdownParser.new(renderer) }
+  let(:renderer) { Renderers::PostBodyHtmlRenderer.new }
+  let(:base_url) { "http://localhost:5000" }
+
+  def normalize_html(str)
+    str.gsub("\n", '').squeeze(' ')
+  end
+
+  def assert(markdown, html)
+    expect(normalize_html(parser.render(markdown))).to match html
+  end
+
+  describe 'render' do
+
+    it 'should render basic markdown' do
+      markdown = "# Greetings!\n\nWelcome to the test suite."
+      html = %r{<h1>Greetings!</h1><p>Welcome to the test suite.</p>}
+      assert markdown, html
+    end
+
+    it 'should render emojis' do
+      markdown = ":chicken:"
+      html = %r{<p><img class="emoji chicken-emoji" alt="chicken" src="http://.*\.png" /></p>}
+      assert markdown, html
+    end
+
+    it 'should resolve links to absolute URLS' do
+      markdown = "<a href='/first'>First</a><a href=\"/second\">Second</a>"
+      html = %r{<p><a href='#{ base_url }/first'>First</a><a href="#{ base_url }/second">Second</a></p>}
+      assert markdown, html
+    end
+
+  end
+end
