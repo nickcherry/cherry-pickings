36128c0cdcbe6dafd6baa8667de9a81538f6fc67
Nick Cherry <ncherry@cipherhealth.com>
2015-11-15 18:45:04 -0500

    make link and asset spec less flaky

--- spec/features/post_links_and_assets_spec.rb
+++ spec/features/post_links_and_assets_spec.rb
@@ -10,6 +10,7 @@ describe 'Post internal links and assets', type: :feature, js: true do
   it 'should all return healthy status codes' do
     rake 'posts:sync'
     expect(Post.count).to be > 0 # sanity check
+    visit '/' # ensure server availability
     Post.all.each do |post|
       URI.extract(post.body_html, ['http', 'https']).each do |link|
         expect(Net::HTTP.get_response(URI(link)).code).to match /^(2|3)/
